13bfac4d1e86b61b96536521b3b0884dd61ca696
Anjan Roy <anjanroy@yandex.com>
2022-02-22 05:23:46 +0000

    populate allocation with random field elements

--- include/common.hpp
+++ include/common.hpp
@@ -1,6 +1,9 @@
 #pragma once
+#include "ff.hpp"
 #include <random>
 
+// Fill host accessible memory allocation with `dim` -many
+// random integers, uniformly chosen from [-3, 4]
 void
 random_fill(double* const data, const size_t dim)
 {
@@ -10,5 +13,19 @@ random_fill(double* const data, const size_t dim)
 
   for (size_t i = 0; i < dim; i++) {
     data[i] = static_cast<double>(dis(gen));
+  }
+}
+
+// Fill host accessible memory allocation with `dim` -many
+// random prime field elements, uniformly chosen from [1, 12289)
+void
+random_fill(uint32_t* const data, const size_t dim)
+{
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<uint32_t> dis(1, ff::Q - 1);
+
+  for (size_t i = 0; i < dim; i++) {
+    data[i] = dis(gen);
   }
 }
