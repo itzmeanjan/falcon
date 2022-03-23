426ceb4682cefa1f6c059b872adc0595b734e4c2
Anjan Roy <anjanroy@yandex.com>
2022-03-23 11:51:58 +0000

    two utility funcitons implemented
    - convert polynomial to canonical form ( ready to apply NTT )
    - check if NTT form of a polynomial is non-zero or not

--- include/utils.hpp
+++ include/utils.hpp
@@ -27,4 +27,75 @@ extract_real_from_complex(sycl::queue& q,
   });
 }
 
+// Given a polynomial generated using `ntru::gen_poly`, such that each randomly
+// sampled coefficient is a double precision floating point number, this
+// function converts each negative coefficient to it's canonical representation
+// in Zq | q = 12289, using `ff:neg` function
+//
+// If a coefficient is >= 0., it's just casted to proper data type ( uint32_t )
+// so that it can be consumed by cooley-tukey NTT compute routine !
+sycl::event
+to_canonical_zq_coefficients(sycl::queue& q,
+                             const double* const __restrict poly_in,
+                             uint32_t* const __restrict poly_out,
+                             const size_t len,
+                             const size_t wg_size,
+                             std::vector<sycl::event> evts)
+{
+  assert(len % wg_size == 0);
+
+  return q.submit([&](sycl::handler& h) {
+    h.depends_on(evts);
+    h.parallel_for(sycl::nd_range<1>{ len, wg_size }, [=](sycl::nd_item<1> it) {
+      const size_t idx = it.get_global_linear_id();
+      const double elm = poly_in[idx];
+
+      if (elm < 0.) {
+        // compute canonical form only when element is negative
+        poly_out[idx] = ff::neg(static_cast<uint32_t>(0. - elm));
+      } else {
+        // otherwise just cast to NTT consumable data type
+        poly_out[idx] = static_cast<uint32_t>(elm);
+      }
+    });
+  });
+}
+
+// Given a polynomial in NTT representation, this function checks whether all
+// coefficients are non-zero or not.
+//
+// Ensure that `nonzero_cnt` argument is initialised to `1`; if after completion
+// of this offloaded compute job, it's found that all coefficients are nonzero,
+// `nonzero_cnt` should hold still `1`, otherwise it'll be zeroed.
+//
+// This implementation uses atomic operation !
+sycl::event
+is_nonzero_coeff(
+  sycl::queue& q,
+  const uint32_t* const __restrict poly,
+  const size_t len,
+  const size_t wg_size,
+  uint8_t* const __restrict nonzero_cnt, // must be initialised to `1` ( true )
+  std::vector<sycl::event> evts)
+{
+  assert(len % wg_size == 0);
+
+  return q.submit([&](sycl::handler& h) {
+    h.depends_on(evts);
+    h.parallel_for(sycl::nd_range<1>{ len, wg_size }, [=](sycl::nd_item<1> it) {
+      const size_t idx = it.get_global_linear_id();
+      const uint32_t elm = poly[idx];
+
+      sycl::ext::oneapi::atomic_ref<
+        uint8_t,
+        sycl::memory_order_relaxed,
+        sycl::memory_scope_device,
+        sycl::access::address_space::ext_intel_global_device_space>
+        nonzero_cnt_ref{ nonzero_cnt[0] };
+
+      nonzero_cnt_ref.fetch_and(static_cast<uint8_t>(elm != 0u));
+    });
+  });
+}
+
 }
