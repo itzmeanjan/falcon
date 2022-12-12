6d58162909d908c1361268be9a008d2ba8640e72
Anjan Roy <hello@itzmeanjan.in>
2022-12-12 15:45:36 +0400

    remove dependency of SYCL from testing prime field arithmetic
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/test_ff.hpp
+++ include/test_ff.hpp
@@ -5,52 +5,41 @@
 
 namespace test {
 
-void
-ff_math(sycl::queue& q)
+inline void
+ff_math()
 {
   const size_t len = ff::Q;
   const size_t size = sizeof(uint32_t) * len;
 
-  uint32_t* in_a = static_cast<uint32_t*>(sycl::malloc_shared(size, q));
-  uint32_t* in_b = static_cast<uint32_t*>(sycl::malloc_shared(size, q));
-  uint32_t* out_sub = static_cast<uint32_t*>(sycl::malloc_shared(size, q));
-  uint32_t* out_neg = static_cast<uint32_t*>(sycl::malloc_shared(size, q));
-  uint32_t* out_mul = static_cast<uint32_t*>(sycl::malloc_shared(size, q));
-  uint32_t* out_div = static_cast<uint32_t*>(sycl::malloc_shared(size, q));
+  uint32_t* in_a = static_cast<uint32_t*>(std::malloc(size));
+  uint32_t* in_b = static_cast<uint32_t*>(std::malloc(size));
+  uint32_t* out_sub = static_cast<uint32_t*>(std::malloc(size));
+  uint32_t* out_neg = static_cast<uint32_t*>(std::malloc(size));
+  uint32_t* out_mul = static_cast<uint32_t*>(std::malloc(size));
+  uint32_t* out_div = static_cast<uint32_t*>(std::malloc(size));
 
-  random_fill(in_a, len);
-  random_fill(in_b, len);
+  random_fill<uint32_t>(in_a, len);
+  random_fill<uint32_t>(in_b, len);
 
-  sycl::event evt0 = q.memset(out_sub, 0, size);
-  sycl::event evt1 = q.memset(out_neg, 0, size);
-  sycl::event evt2 = q.memset(out_mul, 0, size);
-  sycl::event evt3 = q.memset(out_div, 0, size);
-  sycl::event evt4 = q.submit([&](sycl::handler& h) {
-    h.depends_on({ evt0, evt1, evt2, evt3 });
+  for (size_t i = 0; i < len; i++) {
+    out_sub[i] = ff::sub(in_a[i], in_b[i]);
+    out_neg[i] = ff::neg(in_b[i]);
 
-    h.single_task([=]() {
-      for (size_t i = 0; i < len; i++) {
-        out_sub[i] = ff::sub(in_a[i], in_b[i]);
-        out_neg[i] = ff::neg(in_b[i]);
-
-        out_mul[i] = ff::mul(in_a[i], in_b[i]);
-        out_div[i] = ff::div(out_mul[i], in_a[i]);
-      }
-    });
-  });
-  evt4.wait();
+    out_mul[i] = ff::mul(in_a[i], in_b[i]);
+    out_div[i] = ff::div(out_mul[i], in_a[i]);
+  }
 
   for (size_t i = 0; i < len; i++) {
     assert(ff::add(in_a[i], out_neg[i]) == out_sub[i]);
     assert(in_b[i] == out_div[i]);
   }
 
-  sycl::free(in_a, q);
-  sycl::free(in_b, q);
-  sycl::free(out_sub, q);
-  sycl::free(out_neg, q);
-  sycl::free(out_mul, q);
-  sycl::free(out_div, q);
+  std::free(in_a);
+  std::free(in_b);
+  std::free(out_sub);
+  std::free(out_neg);
+  std::free(out_mul);
+  std::free(out_div);
 }
 
 }
