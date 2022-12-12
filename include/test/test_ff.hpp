5ab675d5f9ab348080b8ed581d69f31ff09893fd
Anjan Roy <hello@itzmeanjan.in>
2022-12-12 16:55:27 +0400

    test functional correctness of Falcon field arithmetic
    See https://github.com/itzmeanjan/kyber/blob/bfd9bbbbea6ee064c92bec212cef6058db5c1139/include/test/test_ff.hpp for source of inspiration
    
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/test/test_ff.hpp
+++ include/test/test_ff.hpp
@@ -1,45 +1,62 @@
 #pragma once
-#include "common.hpp"
 #include "ff.hpp"
 #include <cassert>
 
-namespace test {
+// Test functional correctness of Falcon PQC suite implementation
+namespace test_falcon {
 
-inline void
-ff_math()
+// Test functional correctness of Falcon prime field operations, by running
+// through multiple rounds ( see template parameter ) of execution of field
+// arithmetic, on randomly sampled field elements
+template<const size_t rounds = 1024ul>
+static void
+test_field_ops()
 {
-  const size_t len = ff::Q;
-  const size_t size = sizeof(uint32_t) * len;
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<size_t> dis{ 0ul, 1ul << 24 };
 
-  uint32_t* in_a = static_cast<uint32_t*>(std::malloc(size));
-  uint32_t* in_b = static_cast<uint32_t*>(std::malloc(size));
-  uint32_t* out_sub = static_cast<uint32_t*>(std::malloc(size));
-  uint32_t* out_neg = static_cast<uint32_t*>(std::malloc(size));
-  uint32_t* out_mul = static_cast<uint32_t*>(std::malloc(size));
-  uint32_t* out_div = static_cast<uint32_t*>(std::malloc(size));
+  for (size_t i = 0; i < rounds; i++) {
+    const auto a = ff::ff_t::random();
+    const auto b = ff::ff_t::random();
 
-  random_fill<uint32_t>(in_a, len);
-  random_fill<uint32_t>(in_b, len);
+    // addition, subtraction, negation
+    const auto c = a - b;
+    const auto d = -b;
+    const auto e = a + d;
 
-  for (size_t i = 0; i < len; i++) {
-    out_sub[i] = ff::sub(in_a[i], in_b[i]);
-    out_neg[i] = ff::neg(in_b[i]);
+    assert(c == e);
 
-    out_mul[i] = ff::mul(in_a[i], in_b[i]);
-    out_div[i] = ff::div(out_mul[i], in_a[i]);
+    // multiplication, division, inversion
+    const auto f = a * b;
+    const auto g = f / b;
+
+    if (b == ff::ff_t::zero()) {
+      assert(g == ff::ff_t::zero());
+    } else {
+      assert(g == a);
+    }
+
+    const auto h = a.inv();
+    const auto k = h * a;
+
+    if (a == ff::ff_t::zero()) {
+      assert(k == ff::ff_t::zero());
+    } else {
+      assert(k == ff::ff_t::one());
+    }
+
+    // exponentiation, multiplication
+    const size_t exp = dis(gen);
+    const auto l = a ^ exp;
+
+    auto res = ff::ff_t::one();
+    for (size_t j = 0; j < exp; j++) {
+      res = res * a;
+    }
+
+    assert(res == l);
   }
-
-  for (size_t i = 0; i < len; i++) {
-    assert(ff::add(in_a[i], out_neg[i]) == out_sub[i]);
-    assert(in_b[i] == out_div[i]);
-  }
-
-  std::free(in_a);
-  std::free(in_b);
-  std::free(out_sub);
-  std::free(out_neg);
-  std::free(out_mul);
-  std::free(out_div);
 }
 
 }
