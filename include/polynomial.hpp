d34655dd7c3e412ff659f92d6ab235e8ec3353e9
Anjan Roy <hello@itzmeanjan.in>
2022-12-29 17:02:08 +0400

    polynomial multiplication/ division over prime field Z_q | q = 12289
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/polynomial.hpp
+++ include/polynomial.hpp
@@ -1,401 +1,43 @@
 #pragma once
-#include "fft.hpp"
+#include "ff.hpp"
 #include "ntt.hpp"
 
+// Polynomial arithmetic over Falcon Prime Field Z_q | q = 3 * (2 ^ 12) + 1
 namespace polynomial {
 
-class kernelAddRealCoeffPolynomials;
-class kernelNegateRealPolynomial;
-class kernelSubRealCoeffPolynomials;
-class kernelMulRealCoeffPolynomials;
-class kernelDivRealCoeffPolynomials;
-
-class kernelAddZqCoeffPolynomials;
-class kernelNegateZqPolynomial;
-class kernelSubZqCoeffPolynomials;
-class kernelMulZqCoeffPolynomials;
-class kernelDivZqCoeffPolynomials;
-
-// Adds two coefficient representation polynomials over R; see
-// https://github.com/tprest/falcon.py/blob/3a6fe63db658ff88fbdcb52c0899fe171b86370a/fft.py#L96-L100
-sycl::event
-add(sycl::queue& q,
-    const double* const __restrict src_a,
-    const size_t len_a,
-    const double* const __restrict src_b,
-    const size_t len_b,
-    double* const __restrict dst,
-    const size_t len_dst,
-    const size_t wg_size,
-    std::vector<sycl::event> evts)
+// Multiply two degree-{(1 << lg2n) - 1} polynomials in their NTT form, by
+// performing element-wise multiplication over Z_q
+template<const size_t lg2n>
+inline void
+mul(const ff::ff_t* const __restrict polya,
+    const ff::ff_t* const __restrict polyb,
+    ff::ff_t* const __restrict polyc)
 {
-  assert(len_a == len_b);
-  assert(len_b == len_dst);
+  constexpr size_t n = 1ul << lg2n;
 
-  sycl::event evt = q.submit([&](sycl::handler& h) {
-    h.depends_on(evts);
-
-    h.parallel_for<kernelAddRealCoeffPolynomials>(
-      sycl::nd_range<1>{ sycl::range<1>{ len_dst }, sycl::range<1>{ wg_size } },
-      [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        dst[idx] = src_a[idx] + src_b[idx];
-      });
-  });
-
-  return evt;
+  for (size_t i = 0; i < n; i++) {
+    polyc[i] = polya[i] * polyb[i];
+  }
 }
 
-// Adds two coefficient representation polynomials over Zq; see
-// https://github.com/tprest/falcon.py/blob/88d01ede1d7fa74a8392116bc5149dee57af93f2/ntt.py#L100-L104
-sycl::event
-add(sycl::queue& q,
-    const uint32_t* const __restrict src_a,
-    const size_t len_a,
-    const uint32_t* const __restrict src_b,
-    const size_t len_b,
-    uint32_t* const __restrict dst,
-    const size_t len_dst,
-    const size_t wg_size,
-    std::vector<sycl::event> evts)
-{
-  assert(len_a == len_b);
-  assert(len_b == len_dst);
-
-  sycl::event evt = q.submit([&](sycl::handler& h) {
-    h.depends_on(evts);
-
-    h.parallel_for<kernelAddZqCoeffPolynomials>(
-      sycl::nd_range<1>{ sycl::range<1>{ len_dst }, sycl::range<1>{ wg_size } },
-      [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        dst[idx] = ff::add(src_a[idx], src_b[idx]);
-      });
-  });
-
-  return evt;
-}
-
-// Negates coefficient representation polynomial over R; see
-// https://github.com/tprest/falcon.py/blob/3a6fe63db658ff88fbdcb52c0899fe171b86370a/fft.py#L103-L106
-sycl::event
-neg(sycl::queue& q,
-    const double* const __restrict src,
-    const size_t len_src,
-    double* const __restrict dst,
-    const size_t len_dst,
-    const size_t wg_size,
-    std::vector<sycl::event> evts)
-{
-  assert(len_src == len_dst);
-
-  sycl::event evt = q.submit([&](sycl::handler& h) {
-    h.depends_on(evts);
-
-    h.parallel_for<kernelNegateRealPolynomial>(
-      sycl::nd_range<1>{ sycl::range<1>{ len_dst }, sycl::range<1>{ wg_size } },
-      [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        dst[idx] = -src[idx];
-      });
-  });
-
-  return evt;
-}
-
-// Negates coefficient/ NTT representation polynomial over Zq; see
-// https://github.com/tprest/falcon.py/blob/88d01ede1d7fa74a8392116bc5149dee57af93f2/ntt.py#L107-L110
-sycl::event
-neg(sycl::queue& q,
-    const uint32_t* const __restrict src,
-    const size_t len_src,
-    uint32_t* const __restrict dst,
-    const size_t len_dst,
-    const size_t wg_size,
-    std::vector<sycl::event> evts)
-{
-  assert(len_src == len_dst);
-
-  sycl::event evt = q.submit([&](sycl::handler& h) {
-    h.depends_on(evts);
-
-    h.parallel_for<kernelNegateZqPolynomial>(
-      sycl::nd_range<1>{ sycl::range<1>{ len_dst }, sycl::range<1>{ wg_size } },
-      [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        dst[idx] = ff::neg(src[idx]);
-      });
-  });
-
-  return evt;
-}
-
-// Subtracts coefficient representation polynomials over R; see
-// https://github.com/tprest/falcon.py/blob/3a6fe63db658ff88fbdcb52c0899fe171b86370a/fft.py#L109-L111
-sycl::event
-sub(sycl::queue& q,
-    const double* const __restrict src_a,
-    const size_t len_a,
-    const double* const __restrict src_b,
-    const size_t len_b,
-    double* const __restrict dst,
-    const size_t len_dst,
-    const size_t wg_size,
-    std::vector<sycl::event> evts)
-{
-  assert(len_a == len_dst);
-  assert(len_b == len_dst);
-
-  sycl::event evt = q.submit([&](sycl::handler& h) {
-    h.depends_on(evts);
-
-    h.parallel_for<kernelSubRealCoeffPolynomials>(
-      sycl::nd_range<1>{ sycl::range<1>{ len_dst }, sycl::range<1>{ wg_size } },
-      [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        dst[idx] = src_a[idx] - src_b[idx];
-      });
-  });
-
-  return evt;
-}
-
-// Subtracts coefficient/ NTT representation polynomials over Zq; see
-// https://github.com/tprest/falcon.py/blob/88d01ede1d7fa74a8392116bc5149dee57af93f2/ntt.py#L113-L115
-sycl::event
-sub(sycl::queue& q,
-    const uint32_t* const __restrict src_a,
-    const size_t len_a,
-    const uint32_t* const __restrict src_b,
-    const size_t len_b,
-    uint32_t* const __restrict dst,
-    const size_t len_dst,
-    const size_t wg_size,
-    std::vector<sycl::event> evts)
-{
-  assert(len_a == len_dst);
-  assert(len_b == len_dst);
-
-  sycl::event evt = q.submit([&](sycl::handler& h) {
-    h.depends_on(evts);
-
-    h.parallel_for<kernelSubZqCoeffPolynomials>(
-      sycl::nd_range<1>{ sycl::range<1>{ len_dst }, sycl::range<1>{ wg_size } },
-      [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        dst[idx] = ff::sub(src_a[idx], src_b[idx]);
-      });
-  });
-
-  return evt;
-}
-
-// Multiplies two coefficient representation polynomials over R; see
-// https://github.com/tprest/falcon.py/blob/3a6fe63db658ff88fbdcb52c0899fe171b86370a/fft.py#L114-L116
-std::vector<sycl::event>
-mul(sycl::queue& q,
-    const double* const __restrict src_a_coeff, // input polynomial 1
-    fft::cmplx* const __restrict src_a_fft,
-    const size_t len_a,
-    const double* const __restrict src_b_coeff, // input polynomial 2
-    fft::cmplx* const __restrict src_b_fft,
-    const size_t len_b,
-    fft::cmplx* const __restrict dst_coeff, // result is here
-    fft::cmplx* const __restrict dst_fft,
-    const size_t len_dst,
-    const size_t wg_size,
-    std::vector<sycl::event> evts)
-{
-  assert(len_a == len_dst);
-  assert(len_b == len_dst);
-
-  std::vector<sycl::event> evts0 =
-    fft::cooley_tukey_fft(q, src_a_coeff, src_a_fft, len_a, wg_size, evts);
-  std::vector<sycl::event> evts1 =
-    fft::cooley_tukey_fft(q, src_b_coeff, src_b_fft, len_b, wg_size, evts);
-
-  sycl::event evt = q.submit([&](sycl::handler& h) {
-    h.depends_on({ evts0.at(evts0.size() - 1), evts1.at(evts1.size() - 1) });
-
-    h.parallel_for<kernelMulRealCoeffPolynomials>(
-      sycl::nd_range<1>{ sycl::range<1>{ len_dst }, sycl::range<1>{ wg_size } },
-      [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        dst_fft[idx] = src_a_fft[idx] * src_b_fft[idx];
-      });
-  });
-
-  std::vector<sycl::event> evts2 =
-    fft::cooley_tukey_ifft(q, dst_fft, dst_coeff, len_dst, wg_size, { evt });
-  return evts2;
-}
-
-// Multiplies two coefficient representation polynomials over Zq; see
-// https://github.com/tprest/falcon.py/blob/88d01ede1d7fa74a8392116bc5149dee57af93f2/ntt.py#L118-L120
-std::vector<sycl::event>
-mul(sycl::queue& q,
-    const uint32_t* const __restrict src_a_coeff, // input polynomial 1
-    uint32_t* const __restrict src_a_fft,
-    const size_t len_a,
-    const uint32_t* const __restrict src_b_coeff, // input polynomial 2
-    uint32_t* const __restrict src_b_fft,
-    const size_t len_b,
-    uint32_t* const __restrict dst_coeff, // result is here
-    uint32_t* const __restrict dst_fft,
-    const size_t len_dst,
-    const size_t wg_size,
-    std::vector<sycl::event> evts)
-{
-  assert(len_a == len_dst);
-  assert(len_b == len_dst);
-
-  std::vector<sycl::event> evts0 =
-    ntt::cooley_tukey_ntt(q, src_a_coeff, src_a_fft, len_a, wg_size, evts);
-  std::vector<sycl::event> evts1 =
-    ntt::cooley_tukey_ntt(q, src_b_coeff, src_b_fft, len_b, wg_size, evts);
-
-  sycl::event evt = q.submit([&](sycl::handler& h) {
-    h.depends_on({ evts0.at(evts0.size() - 1), evts1.at(evts1.size() - 1) });
-
-    h.parallel_for<kernelMulZqCoeffPolynomials>(
-      sycl::nd_range<1>{ sycl::range<1>{ len_dst }, sycl::range<1>{ wg_size } },
-      [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        dst_fft[idx] = ff::mul(src_a_fft[idx], src_b_fft[idx]);
-      });
-  });
-
-  std::vector<sycl::event> evts2 =
-    ntt::cooley_tukey_intt(q, dst_fft, dst_coeff, len_dst, wg_size, { evt });
-  return evts2;
-}
-
-// Performs division of two coefficient representation polynomials over R; see
-// https://github.com/tprest/falcon.py/blob/3a6fe63db658ff88fbdcb52c0899fe171b86370a/fft.py#L119-L121
-std::vector<sycl::event>
-div(sycl::queue& q,
-    const double* const __restrict src_a_coeff, // input polynomial 1
-    fft::cmplx* const __restrict src_a_fft,
-    const size_t len_a,
-    const double* const __restrict src_b_coeff, // input polynomial 2
-    fft::cmplx* const __restrict src_b_fft,
-    const size_t len_b,
-    fft::cmplx* const __restrict dst_coeff, // result is here
-    fft::cmplx* const __restrict dst_fft,
-    const size_t len_dst,
-    const size_t wg_size,
-    std::vector<sycl::event> evts)
-{
-  assert(len_a == len_dst);
-  assert(len_b == len_dst);
-
-  std::vector<sycl::event> evts0 =
-    fft::cooley_tukey_fft(q, src_a_coeff, src_a_fft, len_a, wg_size, evts);
-  std::vector<sycl::event> evts1 =
-    fft::cooley_tukey_fft(q, src_b_coeff, src_b_fft, len_b, wg_size, evts);
-
-  sycl::event evt = q.submit([&](sycl::handler& h) {
-    h.depends_on({ evts0.at(evts0.size() - 1), evts1.at(evts1.size() - 1) });
-
-    h.parallel_for<kernelDivRealCoeffPolynomials>(
-      sycl::nd_range<1>{ sycl::range<1>{ len_dst }, sycl::range<1>{ wg_size } },
-      [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        dst_fft[idx] = src_a_fft[idx] / src_b_fft[idx];
-      });
-  });
-
-  std::vector<sycl::event> evts2 =
-    fft::cooley_tukey_ifft(q, dst_fft, dst_coeff, len_dst, wg_size, { evt });
-  return evts2;
-}
-
-// Performs division of two coefficient representation polynomials over Zq; see
-// https://github.com/tprest/falcon.py/blob/88d01ede1d7fa74a8392116bc5149dee57af93f2/ntt.py#L123-L128
-std::vector<sycl::event>
-div(sycl::queue& q,
-    const uint32_t* const __restrict src_a_coeff, // input polynomial 1
-    uint32_t* const __restrict src_a_fft,
-    const size_t len_a,
-    const uint32_t* const __restrict src_b_coeff, // input polynomial 2
-    uint32_t* const __restrict src_b_fft,
-    const size_t len_b,
-    uint32_t* const __restrict dst_coeff, // result is here
-    uint32_t* const __restrict dst_fft,
-    const size_t len_dst,
-    const size_t wg_size,
-    std::vector<sycl::event> evts)
-{
-  assert(len_a == len_dst);
-  assert(len_b == len_dst);
-
-  std::vector<sycl::event> evts0 =
-    ntt::cooley_tukey_ntt(q, src_a_coeff, src_a_fft, len_a, wg_size, evts);
-  std::vector<sycl::event> evts1 =
-    ntt::cooley_tukey_ntt(q, src_b_coeff, src_b_fft, len_b, wg_size, evts);
-
-  sycl::event evt = q.submit([&](sycl::handler& h) {
-    h.depends_on({ evts0.at(evts0.size() - 1), evts1.at(evts1.size() - 1) });
-
-    h.parallel_for<kernelDivZqCoeffPolynomials>(
-      sycl::nd_range<1>{ sycl::range<1>{ len_dst }, sycl::range<1>{ wg_size } },
-      [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        dst_fft[idx] = ff::div(src_a_fft[idx], src_b_fft[idx]);
-      });
-  });
-
-  std::vector<sycl::event> evts2 =
-    ntt::cooley_tukey_intt(q, dst_fft, dst_coeff, len_dst, wg_size, { evt });
-  return evts2;
-}
-
-// Computes adjoint of a polynomial provided in coefficient representation
+// Divide one degree-{(1 << lg2n) - 1} polynomial by another one, in their NTT
+// form, by performing element-wise division over Z_q
 //
-// See
-// https://github.com/tprest/falcon.py/blob/88d01ede1d7fa74a8392116bc5149dee57af93f2/fft.py#L124-L126
-std::vector<sycl::event>
-adj(sycl::queue& q,
-    const double* const __restrict s_coeff, // input is here
-    fft::cmplx* const __restrict s_fft,
-    const size_t s_len,
-    fft::cmplx* const __restrict d_coeff, // output is here
-    fft::cmplx* const __restrict d_fft,
-    const size_t d_len,
-    const size_t wg_size,
-    std::vector<sycl::event> evts)
+// Note, because multiplicative inverse of additive identity element ( i.e. 0 )
+// can't be computed, attempt to divide by 0 over Z_q, should result in 0. As
+// this implementation doesn't emit any kind of exceptions, it might be little
+// problematic when using these APIs.
+template<const size_t lg2n>
+inline void
+div(const ff::ff_t* const __restrict polya,
+    const ff::ff_t* const __restrict polyb,
+    ff::ff_t* const __restrict polyc)
 {
-  using namespace fft;
-  using events = std::vector<sycl::event>;
+  constexpr size_t n = 1ul << lg2n;
 
-  assert(s_len == d_len);
-  assert(s_len % wg_size == 0);
-
-  events evts0 = cooley_tukey_fft(q, s_coeff, s_fft, s_len, wg_size, evts);
-
-  sycl::event evt0 = q.submit([&](sycl::handler& h) {
-    h.depends_on(evts0);
-    h.parallel_for(sycl::nd_range<1>{ s_len, wg_size },
-                   [=](sycl::nd_item<1> it) {
-                     const size_t idx = it.get_global_linear_id();
-
-                     d_fft[idx] = std::conj(s_fft[idx]);
-                   });
-  });
-
-  events evts1 = cooley_tukey_ifft(q, d_fft, d_coeff, d_len, wg_size, { evt0 });
-
-  return evts1;
+  for (size_t i = 0; i < n; i++) {
+    polyc[i] = polya[i] / polyb[i];
+  }
 }
 
 }
