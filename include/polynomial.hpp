eb241407fc746cfdcca16e275b7b44f4e11965ec
Anjan Roy <hello@itzmeanjan.in>
2023-01-01 13:46:33 +0400

    polynomial multiplication in FFT form, over C
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/polynomial.hpp
+++ include/polynomial.hpp
@@ -1,8 +1,25 @@
 #pragma once
+#include "ff.hpp"
+#include "fft.hpp"
 #include "ntt.hpp"
 
 // Polynomial arithmetic over Falcon Prime Field Z_q | q = 3 * (2 ^ 12) + 1
 namespace polynomial {
+
+// Multiply two degree-{(1 << lg2n) - 1} polynomials in their FFT form, by
+// performing element-wise multiplication over C
+template<const size_t lg2n>
+inline void
+mul(const fft::cmplx* const __restrict polya,
+    const fft::cmplx* const __restrict polyb,
+    fft::cmplx* const __restrict polyc)
+{
+  constexpr size_t n = 1ul << lg2n;
+
+  for (size_t i = 0; i < n; i++) {
+    polyc[i] = polya[i] * polyb[i];
+  }
+}
 
 // Multiply two degree-{(1 << lg2n) - 1} polynomials in their NTT form, by
 // performing element-wise multiplication over Z_q
