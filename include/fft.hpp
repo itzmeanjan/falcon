f5b40df1790b91336de9169572a64c40904a8ae9
Anjan Roy <hello@itzmeanjan.in>
2023-01-01 13:40:37 +0400

    add inverse fast fourier transform
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/fft.hpp
+++ include/fft.hpp
@@ -30,10 +30,11 @@ computeζ(const size_t k)
 // Note, this routine mutates input i.e. it's an in-place FFT implementation.
 //
 // Implementation inspired from
-// https://github.com/itzmeanjan/falcon/blob/4ab9f60a0cda5a01b482e89717278418a58e584e/include/ntt.hpp#L59-L98
+// https://github.com/itzmeanjan/falcon/blob/4ab9f60/include/ntt.hpp#L59-L98
 template<const size_t LOG2N>
 inline void
 fft(cmplx* const __restrict vec)
+  requires(ntt::check_log2n(LOG2N))
 {
   constexpr size_t N = 1ul << LOG2N;
 
@@ -53,6 +54,47 @@ fft(cmplx* const __restrict vec)
         vec[i] = vec[i] + tmp;
       }
     }
+  }
+}
+
+// Given {512, 1024} evaluations of polynomial f ∈ Q[x]/(φ) s.t. each evaluation
+// ∈ C and they are placed in bit-reversed order, this routine computes inverse
+// fast fourier transform using Gentleman-Sande algorithm, producing polynomial
+// f s.t. its {512, 1024} coefficients are placed in standard order.
+//
+// Note, this routine mutates input i.e. it's an in-place iFFT implementation.
+//
+// Implementation inspired from
+// https://github.com/itzmeanjan/falcon/blob/4ab9f60/include/ntt.hpp#L59-L98
+template<const size_t LOG2N>
+inline void
+ifft(cmplx* const __restrict vec)
+  requires(ntt::check_log2n(LOG2N))
+{
+  constexpr size_t N = 1ul << LOG2N;
+  constexpr double INV_N = 1. / static_cast<double>(N);
+
+  for (size_t l = 0; l < LOG2N; l++) {
+    const size_t len = 1ul << l;
+    const size_t lenx2 = len << 1;
+    const size_t k_beg = (N >> l) - 1;
+
+    for (size_t start = 0; start < N; start += lenx2) {
+      const size_t k_now = k_beg - (start >> (l + 1));
+      const auto neg_ζ_exp = -computeζ<N>(ntt::bit_rev<LOG2N>(k_now));
+
+      for (size_t i = start; i < start + len; i++) {
+        const auto tmp = vec[i];
+
+        vec[i] = vec[i] + vec[i + len];
+        vec[i + len] = tmp - vec[i + len];
+        vec[i + len] = vec[i + len] * neg_ζ_exp;
+      }
+    }
+  }
+
+  for (size_t i = 0; i < N; i++) {
+    vec[i] = vec[i] * INV_N;
   }
 }
 
