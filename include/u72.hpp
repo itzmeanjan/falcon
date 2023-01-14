4ed653f3164f21a46f0d5442bfcc2eb1731d2b5d
Anjan Roy <hello@itzmeanjan.in>
2023-01-14 11:38:13 +0400

    72 -bit unsigned integer construct from big/ little endian bytes
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/u72.hpp
+++ include/u72.hpp
@@ -1,82 +1,52 @@
 #pragma once
-#include <CL/sycl.hpp>
+#include <array>
+#include <cstddef>
+#include <cstdint>
 
+// Sampler over the Integers requires 72 -bit precision unsigned integer
+// arithmetic and comparsion operations
 namespace u72 {
 
-// 72 -bit unsigned integer required for gausian sampling
-typedef std::array<uint8_t, 9> u72;
-
-static uint32_t
-add_small(u72* x, uint32_t d)
+// 72 -bit unsigned integer type with arithmetic ( +, - ) and comparison ( < )
+// operator defined, built on top of underlying 64 -bit unsigned integers
+struct u72_t
 {
-  for (size_t i = 0; i < (*x).size(); i++) {
-    uint32_t w;
+  uint64_t hi = 0ul;
+  uint64_t lo = 0ul;
 
-    w = (*x)[i] + d;
-    (*x)[i] = w & 0xff;
-    d = w >> 8;
+  inline constexpr u72_t(const uint8_t hi = 0, const uint64_t lo = 0ul)
+  {
+    this->hi = hi;
+    this->lo = lo;
   }
 
-  return d; // carry
-}
+  // Given 9 bytes, this routine computes a 72 -bit unsigned integer s.t. these
+  // bytes are interpreted in little-endian byte order.
+  inline constexpr u72_t from_be_bytes(const std::array<uint8_t, 9> bytes)
+  {
+    const uint8_t hi = bytes[0];
+    uint64_t lo = 0ul;
 
-// Given a 72 -bit unsigned integer as decimal string ( say "12345678" ), this
-// function parses it as 9 bytes
-static u72
-from_decimal(const char* str, const size_t len)
-{
-  u72 x;
-  x.fill(0);
-
-  for (size_t l = 0; l < len; l++) {
-    uint32_t cc = 0;
-
-    for (size_t i = 0; i < x.size(); i++) {
-      uint32_t w;
-
-      w = (x[i] * 10) + cc;
-      x[i] = w & 0xff;
-      cc = w >> 8;
+    for (size_t i = 0; i < 8; i++) {
+      lo |= static_cast<uint64_t>(bytes[1 + i]) << (7 - i) * 8;
     }
 
-    add_small(&x, str[l] - '0'); // '0' = 48 ( ASCII )
+    return u72_t{ hi, lo };
   }
 
-  return x;
-}
+  // Given 9 bytes, this routine computes a 72 -bit unsigned integer s.t. these
+  // bytes are interpreted in big-endian byte order.
+  inline constexpr u72_t from_le_bytes(const std::array<uint8_t, 9> bytes)
+  {
+    const uint8_t hi = bytes[8];
+    uint64_t lo = 0ul;
 
-// Given a byte array of length 9 ( read 72 randomly sampled bits
-// ), this function simply copies byte array content to `u72` backing array,
-// while interpreting input byte array as little endian one
-static u72
-from_bytes(const uint8_t* bytes)
-{
-  u72 x;
-
-  for (size_t l = 0; l < x.size(); l++) {
-    x[8 - l] = bytes[l];
-  }
-
-  return x;
-}
-
-// Compares two 72 -bit unsigned integers and returns
-//
-// -1, if x < y
-//  0, if x == y
-//  1, if x > y
-static int8_t
-cmp(const u72 x, const u72 y)
-{
-  for (int64_t i = x.size() - 1; i >= 0; i--) {
-    if (x[i] > y[i]) {
-      return 1;
-    } else if (x[i] < y[i]) {
-      return -1;
+    for (size_t i = 0; i < 8; i++) {
+      lo |= static_cast<uint64_t>(bytes[i]) << (i * 8);
     }
-  }
 
-  return 0;
-}
+    return u72_t{ hi, lo };
+  }
+};
 
 }
