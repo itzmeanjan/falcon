8d28db34845336173b3655b14b9249d5fed669e9
Anjan Roy <hello@itzmeanjan.in>
2023-01-18 17:09:26 +0400

    instead of taking degree of polynomial `N` as template parameter, take `log2(N)`
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/ntru_gen.hpp
+++ include/ntru_gen.hpp
@@ -9,22 +9,23 @@ namespace ntru_gen {
 // coefficient is sampled from a gaussian distribution D_{Z, σ{f, g}, 0} with σ
 // = 1.17 * √(q/ 8192) as described in equation 3.29 on page 34 of the Falcon
 // specification https://falcon-sign.info/falcon.pdf
-template<const size_t n>
+template<const size_t LOG2N>
 static inline void
 gen_poly(int32_t* const poly)
 {
-  constexpr double σ = 1.43300980528773;
-  constexpr size_t k = 4096 / n;
+  constexpr size_t N = 1ul << LOG2N;
+  constexpr size_t k = 4096 / N;
 
-  for (size_t i = 0; i < n; i++) {
+  constexpr double σ = 1.43300980528773;
+  constexpr double br[]{ samplerz::FALCON512_σ_min,
+                         samplerz::FALCON1024_σ_min };
+  constexpr double σ_min = br[N == 1024];
+
+  for (size_t i = 0; i < N; i++) {
 
     int32_t res = 0;
     for (size_t j = 0; j < k; j++) {
-      if constexpr (n == 512) {
-        res += samplerz::samplerz(0., σ, samplerz::FALCON512_σ_min);
-      } else {
-        res += samplerz::samplerz(0., σ, samplerz::FALCON1024_σ_min);
-      }
+      res += samplerz::samplerz(0., σ, σ_min);
     }
 
     poly[i] = res;
@@ -34,26 +35,24 @@ gen_poly(int32_t* const poly)
 // Given a polynomial of degree (n - 1) | n ∈ {512, 1024}, this routine checks
 // whether it can be inverted by computing NTT representation of polynomial and
 // ensuring none of the coefficients, in NTT representation, are zero.
-template<const size_t n>
+template<const size_t LOG2N>
 static inline bool
 is_poly_invertible(const int32_t* const poly)
 {
+  constexpr size_t N = 1ul << LOG2N;
   constexpr int32_t q = ff::Q;
-  ff::ff_t tmp[n];
 
-  for (size_t i = 0; i < n; i++) {
+  ff::ff_t tmp[N];
+
+  for (size_t i = 0; i < N; i++) {
     const bool flg = poly[i] < 0;
     tmp.v = static_cast<uint16_t>(flg * q + poly[i]);
   }
 
-  if constexpr (n == 512) {
-    ntt::ntt<9>(tmp);
-  } else {
-    ntt::ntt<10>(tmp);
-  }
+  ntt::ntt<LOG2N>(tmp);
 
   bool flg = true;
-  for (size_t i = 0; i < n; i++) {
+  for (size_t i = 0; i < N; i++) {
     flg &= tmp[i].v != 0;
   }
 
@@ -64,13 +63,14 @@ is_poly_invertible(const int32_t* const poly)
 // representation, this routine computes squared norm using formula 3.10, as
 // described on top of page 24 of the Falcon specification
 // https://falcon-sign.info/falcon.pdf
-template<const size_t n>
+template<const size_t LOG2N>
 static inline double
 sqrd_norm(const double* const poly)
 {
+  constexpr size_t N = 1ul << LOG2N;
   double res = 0.;
 
-  for (size_t i = 0; i < n; i++) {
+  for (size_t i = 0; i < N; i++) {
     res += poly[i] * poly[i];
   }
 
@@ -91,7 +91,7 @@ gram_schmidt_norm(const double* const __restrict f,
   constexpr double q = ff::Q;
   constexpr double qxq = q * q;
 
-  const double sq_norm_fg = sqrd_norm<N>(f) + sqrd_norm<N>(g);
+  const auto sq_norm_fg = sqrd_norm<LOG2N>(f) + sqrd_norm<LOG2N>(g);
 
   fft::cmplx f_[N];
   fft::cmplx g_[N];
@@ -139,7 +139,7 @@ gram_schmidt_norm(const double* const __restrict f,
     gt_[i] = std::real(gt[i]);
   }
 
-  const double sq_norm_FG = qxq * (sqrd_norm<N>(ft_) + sqrd_norm<N>(gt_));
+  const auto sq_norm_FG = qxq * (sqrd_norm<LOG2N>(ft_) + sqrd_norm<LOG2N>(gt_));
   return std::max(sq_norm_fg, sq_norm_FG);
 }
 
