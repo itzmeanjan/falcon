7f0889fc0870a5791c17bf5263b77e6694e3e40b
Anjan Roy <hello@itzmeanjan.in>
2023-01-18 16:50:31 +0400

    compute Gram-Schmidt norm of NTRU matrix
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/ntru_gen.hpp
+++ include/ntru_gen.hpp
@@ -1,8 +1,6 @@
 #pragma once
-#include "ff.hpp"
-#include "ntt.hpp"
+#include "polynomial.hpp"
 #include "samplerz.hpp"
-#include <cstring>
 
 // Generate f, g, F, G ∈ Z[x]/(φ) | fG − gF = q mod φ ( i.e. NTRU equation )
 namespace ntru_gen {
@@ -68,7 +66,7 @@ is_poly_invertible(const int32_t* const poly)
 // https://falcon-sign.info/falcon.pdf
 template<const size_t n>
 static inline double
-sqr_norm(const double* const poly)
+sqrd_norm(const double* const poly)
 {
   double res = 0.;
 
@@ -77,6 +75,72 @@ sqr_norm(const double* const poly)
   }
 
   return res;
+}
+
+// Computes squared Gram-Schmidt norm of NTRU matrix generated using random
+// sampled polynomials f, g of degree (N - 1) | N = 2^LOG2N
+//
+// This routine does what line 9 of algorithm 5 in the Falcon specification (
+// https://falcon-sign.info/falcon.pdf ) does.
+template<const size_t LOG2N>
+static inline double
+gram_schmidt_norm(const double* const __restrict f,
+                  const double* const __restrict g)
+{
+  constexpr size_t N = 1ul << LOG2N;
+  constexpr double q = ff::Q;
+  constexpr double qxq = q * q;
+
+  const double sq_norm_fg = sqrd_norm<N>(f) + sqrd_norm<N>(g);
+
+  fft::cmplx f_[N];
+  fft::cmplx g_[N];
+
+  for (size_t i = 0; i < N; i++) {
+    f_[i] = fft::cmplx{ f[i] };
+    g_[i] = fft::cmplx{ g[i] };
+  }
+
+  fft::fft<LOG2N>(f_);
+  fft::fft<LOG2N>(g_);
+
+  fft::cmplx f_adj[N];
+  fft::cmplx g_adj[N];
+
+  std::memcpy(f_adj, f_, sizeof(f_));
+  std::memcpy(g_adj, g_, sizeof(g_));
+
+  fft::adj_poly<LOG2N>(f_adj);
+  fft::adj_poly<LOG2N>(g_adj);
+
+  fft::cmplx fxf_adj[N];
+  fft::cmplx gxg_adj[N];
+
+  polynomial::mul(f, f_adj, fxf_adj);
+  polynomial::mul(g, g_adj, gxg_adj);
+
+  fft::cmplx fxf_adj_gxg_adj[N];
+  polynomial::add<LOG2N>(fxf_adj, gxg_adj, fxf_adj_gxg_adj);
+
+  fft::cmplx ft[N];
+  fft::cmplx gt[N];
+
+  polynomial::div(f_adj, fxf_adj_gxg_adj, ft);
+  polynomial::div(g_adj, fxf_adj_gxg_adj, gt);
+
+  fft::ifft<LOG2N>(ft);
+  fft::ifft<LOG2N>(gt);
+
+  double ft_[N];
+  double gt_[N];
+
+  for (size_t i = 0; i < N; i++) {
+    ft_[i] = std::real(ft[i]);
+    gt_[i] = std::real(gt[i]);
+  }
+
+  const double sq_norm_FG = qxq * (sqrd_norm<N>(ft_) + sqrd_norm<N>(gt_));
+  return std::max(sq_norm_fg, sq_norm_FG);
 }
 
 }
