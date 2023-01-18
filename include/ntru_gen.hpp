dd9291b2d75de4c1a5fc22c4ff80ed5aed895c51
Anjan Roy <hello@itzmeanjan.in>
2023-01-18 15:20:43 +0400

    check if random sampled polynomial is invertible or not
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/ntru_gen.hpp
+++ include/ntru_gen.hpp
@@ -1,5 +1,8 @@
 #pragma once
+#include "ff.hpp"
+#include "ntt.hpp"
 #include "samplerz.hpp"
+#include <cstring>
 
 // Generate f, g, F, G ∈ Z[x]/(φ) | fG − gF = q mod φ ( i.e. NTRU equation )
 namespace ntru_gen {
@@ -10,10 +13,11 @@ namespace ntru_gen {
 // specification https://falcon-sign.info/falcon.pdf
 template<const size_t n>
 static inline void
-gen_poly(int32_t* const poly)
+gen_poly(ff::ff_t* const poly)
 {
   constexpr double σ = 1.43300980528773;
   constexpr size_t k = 4096 / n;
+  constexpr int32_t q = ff::Q;
 
   for (size_t i = 0; i < n; i++) {
 
@@ -26,8 +30,33 @@ gen_poly(int32_t* const poly)
       }
     }
 
-    poly[i] = res;
+    const bool flg = res < 0;
+    poly[i].v = static_cast<uint16_t>(flg * q + res);
   }
+}
+
+// Given a polynomial of degree (n - 1) | n ∈ {512, 1024}, this routine checks
+// whether it can be inverted by computing NTT representation of polynomial and
+// ensuring none of the coefficients, in NTT representation, are zero.
+template<const size_t n>
+static inline bool
+is_poly_invertible(const ff::ff_t* const poly)
+{
+  ff::ff_t tmp[n];
+  std::memcpy(tmp, poly, n * sizeof(ff::ff_t));
+
+  if constexpr (n == 512) {
+    ntt::ntt<9>(tmp);
+  } else {
+    ntt::ntt<10>(tmp);
+  }
+
+  bool flg = true;
+  for (size_t i = 0; i < n; i++) {
+    flg &= tmp[i].v != 0;
+  }
+
+  return flg;
 }
 
 }
