888c53e435555131f4210fb4ec5bce8beaa8628f
Anjan Roy <hello@itzmeanjan.in>
2023-01-25 17:12:07 +0400

    recursive karatsuba for polynomial multiplication
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/karatsuba.hpp
+++ include/karatsuba.hpp
@@ -1,132 +1,68 @@
 #pragma once
-#include <CL/sycl.hpp>
-#include <cassert>
+#include <array>
+#include <gmp.h>
+#include <gmpxx.h>
 
+// Karatsuba Multiplication of two Polynomials
 namespace karatsuba {
 
-class kernelKaratsubaMultiplicationPhase0;
-class kernelKaratsubaMultiplicationPhase1;
-class kernelKaratsubaModularMultiplication;
-
-// Data-parallel Karatsuba polynomial multiplication, inspired from
+// Given two polynomials of degree N-1 ( s.t. N is power of 2 and N >= 1), this
+// routine multiplies them using Karatsuba algorithm, following
 // https://github.com/tprest/falcon.py/blob/88d01ede1d7fa74a8392116bc5149dee57af93f2/ntrugen.py#L14-L39
-// ( recursive ) and https://eprint.iacr.org/2006/224.pdf ( see section 3.2 for
-// iterative implementation )
-std::vector<sycl::event>
-multiplication(sycl::queue& q,
-               const double* const __restrict src_a,
-               const size_t len_a,
-               const double* const __restrict src_b,
-               const size_t len_b,
-               double* const __restrict itmd,
-               const size_t len_itmd,
-               double* const __restrict dst,
-               const size_t len_dst,
-               const size_t wg_size,
-               std::vector<sycl::event> evts)
-{
-  assert(len_a == len_b);
-  assert(len_b == len_itmd);
-  assert((len_itmd << 1) == len_dst);
-  assert(len_itmd % wg_size == 0);        // all workgroups are of same size
-  assert((len_dst & (len_dst - 1)) == 0); // power of 2 many coefficients
-
-  sycl::event evt0 = q.submit([&](sycl::handler& h) {
-    h.depends_on(evts);
-
-    h.parallel_for<kernelKaratsubaMultiplicationPhase0>(
-      sycl::nd_range<1>{ len_itmd, wg_size }, [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        itmd[idx] = src_a[idx] * src_b[idx];
-      });
-  });
-
-  sycl::event evt1 = q.memset(dst, 0, sizeof(double) * len_dst);
-
-  sycl::event evt2 = q.submit([&](sycl::handler& h) {
-    h.depends_on({ evt0, evt1 });
-
-    h.parallel_for<kernelKaratsubaMultiplicationPhase1>(
-      sycl::nd_range<1>{ len_dst, wg_size }, [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
-
-        if (idx == 0) {
-          dst[idx] = itmd[idx];
-        } else if (idx == len_dst - 2) {
-          dst[idx] = itmd[len_itmd - 1];
-        } else {
-          double d_st = 0, d_s_t = 0;
-
-          for (size_t s = 0; s < idx; s++) {
-            size_t t = idx - s;
-
-            if ((len_itmd > t) && (t > s)) {
-              d_st += ((src_a[s] + src_a[t]) * (src_b[s] + src_b[t]));
-              d_s_t += (itmd[s] + itmd[t]);
-            }
-          }
-
-          if ((idx & 0b1) == 0) {
-            // even index
-            dst[idx] = d_st - d_s_t + itmd[idx >> 1];
-          } else {
-            // odd index
-            dst[idx] = d_st - d_s_t;
-          }
-        }
-      });
-  });
-
-  return { evt0, evt1, evt2 };
-}
-
-// Given two polynomials, performs karatsuba multiplication ( see
-// https://github.com/itzmeanjan/falcon/blob/6d2648cb524b8da68ddebb66a95dfaafe1430687/include/karatsuba.hpp#L15-L26
-// ) followed by reduction mod (x ^ n + 1)
+// computing resulting polynomial with degree 2*N - 1
 //
-// See
-// https://github.com/tprest/falcon.py/blob/88d01ede1d7fa74a8392116bc5149dee57af93f2/ntrugen.py#L42-L49
-std::vector<sycl::event>
-modular_multiplication(
-  sycl::queue& q,
-  const double* const __restrict poly_a, // `poly_len` -many coefficients
-  const double* const __restrict poly_b, // `poly_len` -many coefficients
-  double* const __restrict itmd_a,       // `poly_len` -many coefficients
-  double* const __restrict itmd_b,       // `poly_len << 1` -many coefficients
-  double* const __restrict poly_dst,     // `poly_len` -many coefficients
-  const size_t poly_len,
-  const size_t wg_size,
-  std::vector<sycl::event> evts)
+// Note, polynomial coefficients can be big integers - this routine depends on
+// C++ interface of GNU MP
+// https://gmplib.org/manual/C_002b_002b-Interface-Integers
+template<const size_t N>
+static inline std::array<mpz_class, 2 * N>
+karatsuba(const std::array<mpz_class, N>& polya,
+          const std::array<mpz_class, N>& polyb)
 {
-  assert(poly_len % wg_size == 0);
+  static_assert((N & (N - 1)) == 0,
+                "Degree of Polynomial + 1 must be 2^i | i >=0");
 
-  std::vector<sycl::event> evts0 = multiplication(q,
-                                                  poly_a,
-                                                  poly_len,
-                                                  poly_b,
-                                                  poly_len,
-                                                  itmd_a,
-                                                  poly_len,
-                                                  itmd_b,
-                                                  poly_len << 1,
-                                                  wg_size,
-                                                  evts);
+  if constexpr (N == 1) {
+    return { mpz_class(polya[0] * polyb[0]), mpz_class(0) };
+  } else {
+    constexpr size_t Nby2 = N / 2;
 
-  sycl::event evt0 = q.submit([&](sycl::handler& h) {
-    h.depends_on(evts0);
-    h.parallel_for<kernelKaratsubaModularMultiplication>(
-      sycl::nd_range<1>{ poly_len, wg_size }, [=](sycl::nd_item<1> it) {
-        const size_t idx = it.get_global_linear_id();
+    std::array<mpz_class, Nby2> polya0;
+    std::array<mpz_class, Nby2> polya1;
+    std::array<mpz_class, Nby2> polyb0;
+    std::array<mpz_class, Nby2> polyb1;
+    std::array<mpz_class, Nby2> polyax;
+    std::array<mpz_class, Nby2> polybx;
 
-        const double elm0 = itmd_b[idx];
-        const double elm1 = itmd_b[idx + poly_len];
+    for (size_t i = 0; i < Nby2; i++) {
+      polya0[i] = polya[i];
+      polya1[i] = polya[Nby2 + i];
 
-        poly_dst[idx] = elm0 - elm1;
-      });
-  });
+      polyb0[i] = polyb[i];
+      polyb1[i] = polyb[Nby2 + i];
 
-  return { evt0 };
+      polyax[i] = polya[i] + polya[Nby2 + i];
+      polybx[i] = polyb[i] + polyb[Nby2 + i];
+    }
+
+    const std::array<mpz_class, N> polya0b0 = karatsuba<Nby2>(polya0, polyb0);
+    const std::array<mpz_class, N> polya1b1 = karatsuba<Nby2>(polya1, polyb1);
+    std::array<mpz_class, N> polyaxbx = karatsuba<Nby2>(polyax, polybx);
+
+    for (size_t i = 0; i < N; i++) {
+      polyaxbx[i] = polyaxbx[i] - mpz_class(polya0b0[i] + polya1b1[i]);
+    }
+
+    std::array<mpz_class, 2 * N> polyab{};
+
+    for (size_t i = 0; i < N; i++) {
+      polyab[i] = polyab[i] + polya0b0[i];
+      polyab[N + i] = polyab[N + i] + polya1b1[i];
+      polyab[Nby2 + i] = polyab[Nby2 + i] + polyaxbx[i];
+    }
+
+    return polyab;
+  }
 }
 
 }
