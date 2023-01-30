6b9f7cdb411919154285aa4feb0a54d95fbfee75
Anjan Roy <hello@itzmeanjan.in>
2023-01-30 16:22:22 +0400

    computation of Falcon tree from Gram matrix G of dimension 2x2
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/falcon_tree.hpp
+++ include/falcon_tree.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "common.hpp"
 #include "fft.hpp"
 #include "polynomial.hpp"
 #include <cstring>
@@ -32,6 +33,76 @@ ldl(const fft::cmplx* const __restrict G,
   polynomial::mul<log2<N>()>(l10, tmp0, tmp1);
   polynomial::mul<log2<N>()>(tmp1, g00, tmp0);
   polynomial::sub<log2<N>()>(g11, tmp0, d11);
+}
+
+// Given a full-rank Gram matrix G ∈ FFT(Q[x]/ (x^N + 1))^(2×2), this routine
+// computes Falcon tree T ( which is a binary tree ), by recursively splitting
+// diagonal elements of D, which is obtained by repeated LDL* decomposition of
+// G, in recursive manner, following algorithm 9 of Falcon specification
+// https://falcon-sign.info/falcon.pdf.
+//
+// For understanding Falcon tree, you should look at figure 3.1 of specification
+// and read bottom of page 26 of specification. Finally you should also go
+// through section 3.8.3 of specification for understanding how it can be
+// implemented.
+//
+// Note, falcon tree ( of height k ) being a binary tree, can be stored using (1
+// + k) * 2^k complex numbers i.e. ensure that memory allocated under owner T
+// has enough space for storing those many complex numbers. Also note, at
+// deepest level of recursion i.e. when N = 2, only real part of complex number
+// matters i.e. imaginary part is negligibly small.
+template<const size_t N, const size_t AT_LEVEL, const size_t T_HEIGHT>
+static inline void
+ffldl(const fft::cmplx* const __restrict G, fft::cmplx* const __restrict T)
+  requires((N > 1) && ((N & (N - 1)) == 0) && (N <= 1024) &&
+           (AT_LEVEL < T_HEIGHT) && (N == (1ul << (T_HEIGHT - AT_LEVEL))))
+{
+  constexpr size_t node_cnt = 1ul << AT_LEVEL;
+  constexpr size_t tree_off = node_cnt * N;
+
+  fft::cmplx D00[N];
+  fft::cmplx D11[N];
+
+  ldl<N>(G, T, D00, D11);
+
+  if constexpr (N == 2) {
+    // deepest level of recursion !
+    static_assert(AT_LEVEL == (T_HEIGHT - 1),
+                  "Can't go below this level of tree !");
+
+    std::memcpy(T + tree_off, D00, sizeof(D00) / 2);
+    std::memcpy(T + tree_off + (N / 2), D11, sizeof(D11) / 2);
+
+    return;
+  } else {
+    fft::cmplx d00[N / 2];
+    fft::cmplx d01[N / 2];
+    fft::cmplx d10[N / 2];
+    fft::cmplx d11[N / 2];
+
+    fft::split_fft<log2<N>()>(D00, d00, d01);
+    fft::split_fft<log2<N>()>(D11, d10, d11);
+
+    fft::cmplx G0[(N / 2) * 2 * 2];
+    fft::cmplx G1[(N / 2) * 2 * 2];
+
+    std::memcpy(G0, d00, sizeof(d00));
+    std::memcpy(G0 + (N / 2), d01, sizeof(d01));
+    std::memcpy(G0 + N, d01, sizeof(d01));
+    std::memcpy(G0 + N + (N / 2), d00, sizeof(d00));
+    fft::adj_poly<log2<N>()>(G0 + N);
+
+    std::memcpy(G1, d10, sizeof(d10));
+    std::memcpy(G1 + (N / 2), d11, sizeof(d11));
+    std::memcpy(G1 + N, d11, sizeof(d11));
+    std::memcpy(G1 + N + (N / 2), d10, sizeof(d10));
+    fft::adj_poly<log2<N>()>(G1 + N);
+
+    ffldl<N / 2, AT_LEVEL + 1, T_HEIGHT>(G0, T + tree_off);
+    ffldl<N / 2, AT_LEVEL + 1, T_HEIGHT>(G1, T + tree_off + (N / 2));
+
+    return;
+  }
 }
 
 }
