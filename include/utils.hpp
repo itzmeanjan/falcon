c9f3539b898813f6006e34c4e0d3e2e5d87bbbf9
Anjan Roy <hello@itzmeanjan.in>
2023-01-31 17:35:45 +0400

    compile-time compute secret key length for Falcon{512, 1024}
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/utils.hpp
+++ include/utils.hpp
@@ -16,4 +16,22 @@ compute_pkey_len()
          ((N * bw) / 8); // body bytes
 }
 
+// Compile-time compute secret key byte length for Falcon{512, 1024}, following
+// section 3.11.5 of Falcon specification
+template<const size_t N>
+static inline constexpr size_t
+compute_skey_len()
+  requires((N == 512) || (N == 1024))
+{
+  // bit width of coefficients of f, g
+  constexpr size_t br[]{
+    5, // N == 1024
+    6  // N == 512
+  };
+  return 1ul +                      // header byte
+         ((N * br[N == 512]) / 8) + // f as bytes
+         ((N * br[N == 512]) / 8) + // g as bytes
+         N;                         // F as bytes
+}
+
 }
