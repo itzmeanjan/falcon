273f2497ecdd7dbe9d733cfd485ddf288933f582
Anjan Roy <hello@itzmeanjan.in>
2023-02-02 15:46:25 +0400

    hash-to-point routine now also takes salt
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/hashing.hpp
+++ include/hashing.hpp
@@ -5,28 +5,32 @@
 // Message Hashing for Falcon-{512, 1024}
 namespace hashing {
 
-// Given a message byte array of length `mlen` bytes, this function first
-// absorbs it into SHAKE256 XOF state and then computes a degree-(n-1)
-// polynomial over Z_q | q = 12289, by squeezing bytes out of keccak sponge
-// state. Note, n ∈ {512, 1024}.
+// Given uniform random sampled salt bytes ( of length `slen` ) and message of
+// length `mlen` bytes, this function first absorbs salt and message ( in order
+// ) into SHAKE256 XOF state and then computes a degree-(n-1) polynomial over
+// Z_q | q = 12289, by squeezing bytes out of keccak sponge state and rejection
+// sampling.
 //
 // This function is the implementation of algorithm 3, described in section 3.7,
 // on page 31 of Falcon specification https://falcon-sign.info/falcon.pdf
 template<const size_t n>
 inline void
-hash_to_point(const uint8_t* const __restrict msg,
+hash_to_point(const uint8_t* const __restrict salt,
+              const size_t slen,
+              const uint8_t* const __restrict msg,
               const size_t mlen,
               ff::ff_t* const __restrict poly)
+  requires((n == 512) || (n == 1024))
 {
-  static_assert((n == 512) || (n == 1024), "N must ∈ {512, 1024}");
-
   constexpr size_t m = 1ul << 16;
   constexpr size_t q = ff::Q;
   constexpr size_t k = m / q;
   constexpr uint16_t kq = k * q;
 
-  shake256::shake256 hasher{};
-  hasher.hash(msg, mlen);
+  shake256::shake256<true> hasher{};
+  hasher.absorb(salt, slen);
+  hasher.absorb(msg, mlen);
+  hasher.finalize();
 
   size_t i = 0;
   uint8_t buf[2];
