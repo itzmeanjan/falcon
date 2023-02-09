eb7a54c4dcfa63f2d2566d6d7d39d3e2b3a08b28
Anjan Roy <hello@itzmeanjan.in>
2023-02-09 18:08:25 +0400

    benchmark scenario which emulates signing of many messages
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/bench/bench_signing.hpp
+++ include/bench/bench_signing.hpp
@@ -6,13 +6,14 @@
 // Benchmark Falcon PQC suite implementation
 namespace bench_falcon {
 
-// Benchmark Falcon{512, 1024} message signing algorithm.
+// Benchmark Falcon{512, 1024} message signing algorithm, emulating only single
+// message is signed with secret key.
 //
 // Note, this signing API builds matrix B and falcon tree T, everytime it's
 // asked to sign a message, by decoding Falcon secret key.
 template<const size_t N>
 void
-sign(benchmark::State& state)
+sign_single(benchmark::State& state)
   requires((N == 512) || (N == 1024))
 {
   const size_t mlen = state.range();
@@ -48,6 +49,65 @@ sign(benchmark::State& state)
 
   std::free(pkey);
   std::free(skey);
+  std::free(sig);
+  std::free(msg);
+
+  assert(verified);
+}
+
+// Benchmark Falcon{512, 1024} message signing algorithm, emulating many
+// messages are consecutively signed with same secret key.
+//
+// Note, this signing API doesn't build matrix B and falcon tree T, everytime
+// it's asked to sign a message, rather it keeps them loaded in memory. So this
+// benchmark result should be faster compared to above `sign_single` benchmark
+// result.
+template<const size_t N>
+void
+sign_many(benchmark::State& state)
+  requires((N == 512) || (N == 1024))
+{
+  const size_t mlen = state.range();
+
+  constexpr size_t siglen = falcon_utils::compute_sig_len<N>();
+  constexpr size_t matblen = 2 * 2 * N; // 2x2 matrix B = [[g, -f], [G, -F]]
+  constexpr size_t ftlen = (log2<N>() + 1) * (1ul << log2<N>()); // 2^k * (k+1)
+
+  // see table 3.3 of falcon specification
+  constexpr double σ_values[]{ 165.736617183, 168.388571447 };
+  constexpr double σ = σ_values[N == 1024];
+
+  // see table 3.3 of falcon specification
+  constexpr int32_t β2_values[]{ 34034726, 70265242 };
+  constexpr int32_t β2 = β2_values[N == 1024];
+
+  auto B = static_cast<fft::cmplx*>(std::malloc(sizeof(fft::cmplx) * matblen));
+  auto T = static_cast<fft::cmplx*>(std::malloc(sizeof(fft::cmplx) * ftlen));
+  auto h = static_cast<ff::ff_t*>(std::malloc(sizeof(ff::ff_t) * N));
+  auto sig = static_cast<uint8_t*>(std::malloc(siglen));
+  auto msg = static_cast<uint8_t*>(std::malloc(mlen));
+
+  keygen::keygen<N>(B, T, h, σ);
+  random_fill(msg, mlen);
+
+  for (auto _ : state) {
+    falcon::sign<N>(B, T, msg, mlen, sig);
+
+    benchmark::DoNotOptimize(B);
+    benchmark::DoNotOptimize(T);
+    benchmark::DoNotOptimize(msg);
+    benchmark::DoNotOptimize(mlen);
+    benchmark::DoNotOptimize(sig);
+    benchmark::ClobberMemory();
+  }
+
+  state.SetItemsProcessed(static_cast<int64_t>(state.iterations()));
+
+  const bool verified = verification::verify<N, β2>(h, msg, mlen, sig);
+
+  std::free(B);
+  std::free(T);
+  std::free(h);
   std::free(sig);
   std::free(msg);
 
